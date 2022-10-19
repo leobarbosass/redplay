f1d725295d8086969b19fceb8a5a15ba71e226bf
LEONARDO BARBOSA SANTOS <22102366@dsn127m1246728.cfp127aluno.local>
2022-10-19 09:55:20 -0300

    atttt

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// Titulos da api: https://omdbapi.com/?s=thor&page=1&apikey=fc1fef96
+// sinopses: http://www.omdbapi.com/?i=tt3896198&apikey=fc1fef96
+
+const pesquisar = document.getElementById('movie-search-box');
+const listaPesquisada = document.getElementById('search-list');
+const resultado = document.getElementById('result-grid');
+
